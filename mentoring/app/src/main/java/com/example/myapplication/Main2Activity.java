241a6f46c623531a10d440900e2ab59fc715eb04
auddus16 <auddus16@naver.com>
2020-02-11 13:37:17 +0900

    명연commit

--- mentoring/app/src/main/java/com/example/myapplication/Main2Activity.java
+++ mentoring/app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -5,25 +5,34 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends MainActivity {
     Toolbar toolbar;
-    Button btnNext;
-    TextView tvStart;//설문설명 텍스트뷰
+
+    TextView tvName;//사용자 이름
+
+    TextView tvFirst;//<1순위> 당신은
+    TextView tvFirstResult;//1순위 결과값
+    TextView tv12; //입니다. 텍스트뷰
+
+    TextView tvsecond;//<1순위> 당신은
+    TextView tvSecondResult;//1순위 결과값
+    TextView tv; //입니다. 텍스트뷰
+
+    Button btnNext;//next버튼
+
 
     public void setSupportActionBar(@Nullable androidx.appcompat.widget.Toolbar toolbar){
         super.setSupportActionBar(toolbar);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         toolbar =(Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
