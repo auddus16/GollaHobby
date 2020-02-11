94ac0523518fda3cd51e25c7d488087f3a20ed7c
yunjin99 <yunjin3052@naver.com>
2020-02-11 13:41:56 +0900

    윤진commit

--- mentoring/app/src/main/java/com/example/myapplication/Main2Activity.java
+++ mentoring/app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -1,11 +1,15 @@
 package com.example.myapplication;
 
 import androidx.annotation.Nullable;
+<<<<<<< Updated upstream
 import androidx.appcompat.app.ActionBar;
+=======
+>>>>>>> Stashed changes
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+<<<<<<< Updated upstream
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -24,6 +28,21 @@ public class Main2Activity extends MainActivity {
 
     Button btnNext;//next버튼
 
+=======
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+//import android.widget.Toolbar;
+
+import java.util.ArrayList;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private Spinner spinner;
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> arrayAdapter;
+
+    Toolbar toolbar;
+>>>>>>> Stashed changes
 
     public void setSupportActionBar(@Nullable androidx.appcompat.widget.Toolbar toolbar){
         super.setSupportActionBar(toolbar);
@@ -34,8 +53,29 @@ public class Main2Activity extends MainActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+<<<<<<< Updated upstream
         toolbar =(Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+=======
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        arrayList = new ArrayList<>();
+        arrayList.add("10대");
+        arrayList.add("20대");
+        arrayList.add("30대");
+        arrayList.add("40대");
+        arrayList.add("50대");
+        arrayList.add("60대");
+        arrayList.add("70대");
+        arrayList.add("80대");
+        arrayList.add("90대");
+
+        arrayAdapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_spinner_dropdown_item, arrayList);
+        spinner = (Spinner)findViewById(R.id.spinner);
+        spinner.setAdapter(arrayAdapter);
+>>>>>>> Stashed changes
     }
 }
--- mentoring/app/src/main/java/com/example/myapplication/MainActivity.java
+++ mentoring/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,10 +7,16 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+<<<<<<< Updated upstream
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+=======
+import android.os.Handler;
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+>>>>>>> Stashed changes
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
@@ -24,9 +30,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< Updated upstream
 
         toolbar =(Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+=======
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), Main2Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+>>>>>>> Stashed changes
     }
 }
