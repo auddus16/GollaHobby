52c399c581f4907466d76faa3afcb8bcbe8e34ce
auddus16 <auddus16@naver.com>
2020-02-12 13:30:55 +0900

    명연레이아웃수정

--- GollaHobby/app/src/main/java/com/example/myapplication/ResultActivity.java
+++ GollaHobby/app/src/main/java/com/example/myapplication/ResultActivity.java
@@ -1,15 +1,45 @@
 package com.example.myapplication;
 
 // 명연
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultActivity extends AppCompatActivity {
 
+    TextView tvName;
+    TextView tvFirst;
+    TextView tvFirstResult;
+    TextView tvYou1;
+    TextView tvEnd1;
+
+    TextView tvSecond;
+    TextView tvSecondResult;
+    TextView tvYou2;
+    TextView tvEnd2;
+
+    Button btnNext;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        tvName=findViewById(R.id.tvName);
+        tvFirstResult=findViewById(R.id.tvFirstReult);
+        tvSecondResult=findViewById(R.id.tvSecondResult);
+        btnNext=findViewById(R.id.btnNext);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SelectActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
